f40363b3ed9e400f667cce6a4165e69d95f540dc
David Schlachter <david@schlachter.ca>
2020-01-27 15:12:27 -0500

    Add configuration for PM2

---
+++ ecosystem.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	apps: [{
+		name: 'plaintext-notes',
+		script: 'bin/www',
+		watch: 'app.js', 'routes/index.js', 'public/index.html', 'public/javascripts/notes.js', 'public/stylesheets/style.css'],
+		env: {
+			NODE_ENV: 'development'
+		},
+		env_production: {
+			NODE_ENV: 'production'
+		}
+	}]
+};
