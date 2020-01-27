9649daa771a2358cded85d39f25bf50b0a443d82
David Schlachter <david+gh@schlachter.ca>
2020-01-27 15:14:21 -0500

    Fix typo in PM2 configuration file

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,7 +2,7 @@ module.exports = {
 	apps: [{
 		name: 'plaintext-notes',
 		script: 'bin/www',
-		watch: 'app.js', 'routes/index.js', 'public/index.html', 'public/javascripts/notes.js', 'public/stylesheets/style.css'],
+		watch: ['app.js', 'routes/index.js', 'public/index.html', 'public/javascripts/notes.js', 'public/stylesheets/style.css'],
 		env: {
 			NODE_ENV: 'development'
 		},
